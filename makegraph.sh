f044734f2b906d02721edb48d413716631801079
Håvard Gulldahl <havard@gulldahl.no>
2014-12-17 19:05:44 +0100

    updating rrdtool commands to fit our use case

--- makegraph.sh
+++ makegraph.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # from https://gist.github.com/sergray/1d2572f9646d79711518
 # by http://sergray.me/energy-monitoring-with-python.html
-rrdtool graph kW.png --vertical-label=W*h --font=DEFAULT:11:Arial -w 900 -h 600 \
-DEF:akw=readings.rrd:kW:AVERAGE CDEF:wkw=akw,1000,\* LINE2:wkw#FF0000
+rrdtool graph W.png --vertical-label=W*h --font=DEFAULT:11:Arial -w 900 -h 600 \
+DEF:akw=readings.rrd:WATT:AVERAGE CDEF:wkw=akw,1000,\* LINE2:wkw#FF0000
--- run.sh
+++ run.sh
@@ -22,11 +22,11 @@ if [ ! -f "$RAWDB" ]; then
     # aggregated with 5 minute resolution for last 2 days and 1 hour resolution
     # for 28 days
     rrdtool create "$RAWDB" --step $EFERGY_INTERVAL \
-    DS:kW:GAUGE:120:0:300 \
-    RRA:AVERAGE:0.5:5:576 \
-    RRA:MAX:0.5:5:576 \
-    RRA:AVERAGE:0.5:60:672 \
-    RRA:MAX:0.5:60:672
+    DS:WATT:GAUGE:36:0:3e+04 \
+    RRA:AVERAGE:0.5:10:480 \
+    RRA:MAX:0.5:10:480 \
+    RRA:AVERAGE:0.5:200:672 \
+    RRA:MAX:0.5:200:672
 fi
 
 if [ ! -f "$TARRIFFDB" ]; then
@@ -48,8 +48,8 @@ rtl_fm -f 433.55e6 -s 192e3 -r 96e3 -g $RECEIVER_GAIN -p 25 |
 ./EfergyRPI_001 |
 while read DATE TIME USAGE; 
 do
-    test "$USAGE" || continue; # skip empty readings
+    test "x$USAGE" == "x" && continue; # skip empty readings
     echo "--D: $DATE --T: $TIME --U: $USAGE --";
-    rrdtool update "$RAWDB" N:$USAGE;
+    rrdtool updatev "$RAWDB" N:$USAGE;
 done
     
